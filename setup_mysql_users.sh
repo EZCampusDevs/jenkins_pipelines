c966f88ec11c2c22c7065cd0db8d83bb5c147fbb
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-07-03 23:07:10 -0400

    logs aren't working...

--- setup_mysql_users.sh
+++ setup_mysql_users.sh
@@ -1,31 +1,21 @@
 #!/bin/sh
 
 USE_LOG_FILE_ARGUMENT="USE_LOG_FILE"
+log_dir="$HOME/log/jenkins-ssh"
+mkdir -p $log_dir
+log_file="$log_dir/mysql-users-create-log.out"
+touch $log_file
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$log_file 2>&1
 
 if [ $# != 0 ]; then
-
    echo "Processing start arguments"
-
    for arg in "$@"; do
-
       if [ "$arg" = "$USE_LOG_FILE_ARGUMENT" ]; then
-
          echo "Writing all stdout and stderr to file"
-
-         log_dir="$HOME/log/jenkins-ssh"
-         mkdir -p $log_dir
-
-         log_file="$log_dir/mysql-users-create-log.out"
-         touch $log_file
-
-         exec 3>&1 4>&2
-         trap 'exec 2>&4 1>&3' 0 1 2 3
-         exec 1>$log_file 2>&1
-
       fi
-
    done
-
 fi
 
 # Create a list of usernames and passwords
