352556b0f352fc5ab0d4d5e497bc5c842a728350
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-07-03 23:01:27 -0400

    add logging to users shell

--- setup_mysql_users.sh
+++ setup_mysql_users.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+USE_LOG_FILE_ARGUMENT="USE_LOG_FILE"
+
+if [ $# != 0 ]; then
+
+   echo "Processing start arguments"
+
+   for arg in "$@"; do
+
+      if [ "$arg" = "$USE_LOG_FILE_ARGUMENT" ]; then
+
+         echo "Writing all stdout and stderr to file"
+
+         log_dir="$HOME/log/jenkins-ssh"
+         mkdir -p $log_dir
+
+         log_file="$log_dir/mysql-users-create-log.out"
+         touch $log_file
+
+         exec 3>&1 4>&2
+         trap 'exec 2>&4 1>&3' 0 1 2 3
+         exec 1>$log_file 2>&1
+
+      fi
+
+   done
+
+fi
+
 # Create a list of usernames and passwords
 usernames=("$MYSQL_USERNAME_1" "$MYSQL_USERNAME_2" "$MYSQL_USERNAME_3")
 passwords=("$MYSQL_USER_PASS_1" "$MYSQL_USER_PASS_2" "$MYSQL_USER_PASS_3")
